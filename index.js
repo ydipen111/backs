414b5eb8a496df8a98b678a982915af4c0aa3fc7
Your Name <aggressivebanda74@gmail.com>
2024-12-10 10:36:34 +0545

    onvercel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: true,
+  origin: ['https://fullstack-black-three.vercel.app'],
   // origin: '*',
   credentials: true
 }));
