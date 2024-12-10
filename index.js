8f5894a4cb530165848703432d43d7e141de95ae
Your Name <aggressivebanda74@gmail.com>
2024-12-10 10:46:34 +0545

    commitign

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://backs-0h7q.onrender.com'],
+  origin: ['https://fullstack-black-three.vercel.app/'],
   // origin: '*',
   credentials: true
 }));
